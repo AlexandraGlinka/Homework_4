838daa669f209526a5554c0736e9648a7547b272
glinka <alexandraglinka@yandex.ru>
2023-02-20 22:53:47 +0300

    Task_1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        //task2();
+    }
+    public static void task1 () {
+        System.out.println("Задача 1");
+        //задача 1
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать");
+        }
     }
 }
